b7cdedc5215c7b4818624c56e3b28441a177f844
Michael Patellis <mpatellis14@gmail.com>
2019-09-22 22:19:34 -0400

    Changed gitignore to maybe fix run problems

--- client/src/components/Pieces.js
+++ client/src/components/Pieces.js
@@ -29,7 +29,7 @@ export default function PersistentDrawerRight() {
   const [hasAccount, setHasAccount] = React.useState(true)
   const [isLogedIn, setIsLogedIn] = React.useState(false)
   const [editable, setEditable] = React.useState(true)
-  const [isVerified, setVerification] = React.useState(true)
+  const [isVerified, setVerification] = React.useState(false)
   const [Error, setError] = React.useState({})
 
   var password =''
--- server/config/development.js
+++ server/config/development.js
@@ -1,5 +1,5 @@
 module.exports = {
   db: {
-    url: 'mongodb://localhost/contact_app'//'mongodb://user:user12@ds219078.mlab.com:19078/heroku_dvnrw2pd'
+    url: 'mongodb://user:user12@ds219078.mlab.com:19078/heroku_dvnrw2pd'//'mongodb://localhost/contact_app'
   }
 }
