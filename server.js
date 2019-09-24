54a8362fad444e1d3eeed26c677879a313ae35c9
Michael Patellis <mpatellis14@gmail.com>
2019-09-24 00:07:23 -0400

    deploy please

--- server.js
+++ server.js
@@ -17,8 +17,16 @@ var url = config.db.url
 mongoose.Promise = global.Promise
 mongoose.connect(url, { useNewUrlParser: true })
 
+app.use(express.static(path.join(dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(dirname, "client", "build", "index.html"));
+});
+
 const PORT = config.port
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
+
