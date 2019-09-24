a4405df18f43a7d33b9bb630d467fdc1384d910e
Michael Patellis <mpatellis14@gmail.com>
2019-09-24 00:34:18 -0400

    Reverting back to square 1 ):

--- server.js
+++ server.js
@@ -17,14 +17,7 @@ var url = config.db.url
 mongoose.Promise = global.Promise
 mongoose.connect(url, { useNewUrlParser: true })
 
-app.use(express.static(path.join(dirname, "client", "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(dirname, "client", "build", "index.html"));
-});
-
 const PORT = config.port
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
